6c98dbf8e9ff685bca83784f665f6186c638c296
l.kessler.one@gmail.com <l.kessler.one@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-02-17 12:16:39 +0000

    empty stack class

---
+++ src/larskessler/Stack.java
@@ -0,0 +1,21 @@
+package larskessler;
+
+import java.util.EmptyStackException;
+
+import ch.fhnw.algd2.lesson1.exercise.IStack;
+
+public class Stack<T> implements IStack<T> {
+
+	@Override
+	public T pop() throws EmptyStackException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void push(T o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
