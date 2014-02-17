cd65c267a56d1e8ba740c4f7a3e2809d7695109c
l.kessler.one@gmail.com <l.kessler.one@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-02-17 12:45:12 +0000

    stack fertig

--- src/larskessler/Stack.java
+++ src/larskessler/Stack.java
@@ -5,17 +5,35 @@ import java.util.EmptyStackException;
 import ch.fhnw.algd2.lesson1.exercise.IStack;
 
 public class Stack<T> implements IStack<T> {
+	
+	private Element element;
+	
+	@Override
+	public void push(T o) {
+		// push element to list
+		element = new Element(element, o);
+	}
 
 	@Override
 	public T pop() throws EmptyStackException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void push(T o) {
-		// TODO Auto-generated method stub
+		// check if stack is popable
+		if(element == null) {
+			throw new EmptyStackException();
+		}
 		
+		T tmp = element.current;
+		this.element = element.last;
+		return tmp;
 	}
 
+	public class Element {
+		
+		Element last;
+		T current;
+
+		public Element(Element current, T next) {
+			this.last = current;
+			this.current = next;
+		}
+	}
 }
