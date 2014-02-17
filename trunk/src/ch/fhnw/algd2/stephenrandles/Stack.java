5eb4b1c4a13d62b2f71ee2e755541e4c3c28fe71
stephenruk@gmail.com <stephenruk@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-02-17 12:55:35 +0000

    Initial try at an array-based stack

---
+++ trunk/src/ch/fhnw/algd2/stephenrandles/Stack.java
@@ -0,0 +1,41 @@
+// Created by Stephen Randles 17.02.2014
+
+package ch.fhnw.algd2.stephenrandles;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+import ch.fhnw.algd2.lesson1.exercise.IStack;
+
+public class Stack<T> implements IStack<T> {
+	final private int DEFAULT_SIZE = 100;
+	final private double GROWTH_RATE = 2;
+	
+	private Object[] stack = new Object[DEFAULT_SIZE];
+	private int pointer;
+	
+	@Override
+	public void push(T o) {
+		pointer++;
+		if (pointer == stack.length) {
+			increaseStackSize();
+		}		
+		stack[pointer] = o;
+	}
+
+	@Override
+	public T pop() throws EmptyStackException {
+		if (pointer == 0 && stack[0] == null) {
+			throw new EmptyStackException();
+		}
+
+		return (T)stack[pointer--];
+	}
+	
+	private void increaseStackSize() {
+		int newSize = (int)(stack.length * GROWTH_RATE);
+		stack = Arrays.copyOf(stack, newSize);
+	}
+	
+	
+}
