cf751277b7f79fe390fb3e65fcf3ea8c5ac16aa2
stepbrunner@gmail.com <stepbrunner@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-02-17 13:29:47 +0000

    StackTest changed to asserEquals methods

---
+++ trunk/src/ch/fhnw/algd2/stephanbrunner/Stack.java
@@ -0,0 +1,26 @@
+// Created by Stephan Brunner on 17.02.2014
+
+package ch.fhnw.algd2.stephanbrunner;
+
+import java.util.EmptyStackException;
+import ch.fhnw.algd2.lesson1.exercise.IStack;
+
+public class Stack<T> implements IStack<T> {
+
+
+    /**
+     * Pushes an object o onto the stack.
+     */
+    public void push(T o){
+        
+    }
+    
+    /**
+     * Removes the top element from the stack.
+     * Throws an EmptyStackException if there is no element left.
+     */
+    public T pop() throws EmptyStackException{        
+        return null;
+    }
+    
+}
