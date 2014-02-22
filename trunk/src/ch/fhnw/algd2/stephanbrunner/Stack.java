6643ac3185044aac1aa1e191ae7c5716a8e03f75
stepbrunner@gmail.com <stepbrunner@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-02-22 10:51:56 +0000

    Stack implemented

--- trunk/src/ch/fhnw/algd2/stephanbrunner/Stack.java
+++ trunk/src/ch/fhnw/algd2/stephanbrunner/Stack.java
@@ -3,24 +3,45 @@
 package ch.fhnw.algd2.stephanbrunner;
 
 import java.util.EmptyStackException;
+
 import ch.fhnw.algd2.lesson1.exercise.IStack;
 
 public class Stack<T> implements IStack<T> {
-
-
+    private Node top;
+    
     /**
      * Pushes an object o onto the stack.
      */
+    @Override
     public void push(T o){
-        
+        top = new Node(o, top);
     }
     
     /**
      * Removes the top element from the stack.
      * Throws an EmptyStackException if there is no element left.
      */
-    public T pop() throws EmptyStackException{        
-        return null;
+    @Override
+    public T pop() throws EmptyStackException{
+        if (top == null) 
+            throw new EmptyStackException();
+        T result = top.content;
+        top = top.nextNode;
+        return result;
     }
     
+    /**
+     * Holds an Object and a Reference to the next Node.
+     * 
+     * @author Stephan
+     */
+    private class Node {
+        private T content;
+        private Node nextNode;
+                
+        public Node(T content, Node nextNode) {
+            this.content = content;
+            this.nextNode = nextNode;
+        }
+    }
 }
