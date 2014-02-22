49f883394e1dc8a5e874546ac7fd55caf13c9337
stepbrunner@gmail.com <stepbrunner@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-02-22 13:51:16 +0000

    AbstractLinkedList: back to original state, testclass crashed with Marius's modification : ) LinkedListTest: Now also with org.junit.Assert

--- src/ch/fhnw/algd2/lesson1/exercise/LinkedListTest.java
+++ src/ch/fhnw/algd2/lesson1/exercise/LinkedListTest.java
@@ -2,6 +2,10 @@
 
 package ch.fhnw.algd2.lesson1.exercise;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -12,67 +16,68 @@ import org.junit.Test;
 import ch.fhnw.algd2.Clock;
 
 public class LinkedListTest {
-	
-	private ArrayList<AbstractLinkedList<Integer>> lists;
-	
-	@Before
-	public void setUp() throws Exception {
-		lists = new ArrayList<>();
-		lists.add(new ch.fhnw.algd2.florianfankhauser.LinkedList<Integer>());
-		lists.add(new ch.fhnw.algd2.romangribi.LinkedList<Integer>());
-		lists.add(new ch.fhnw.algd2.christianguedel.LinkedList<Integer>());
-		lists.add(new ch.fhnw.algd2.emanuelmistretta.LinkedList<Integer>());
-		lists.add(new ch.fhnw.algd2.stephenrandles.LinkedList<Integer>());
-		lists.add(new ch.fhnw.algd2.yannickaugstburger.LinkedList<Integer>());
-		lists.add(new ch.fhnw.algd2.mariusdubach.LinkedList<Integer>());
-		lists.add(new ch.fhnw.algd2.luzius.LinkedList<Integer>()); // add your own LinkedList implementation here
-	}
-	
-	@Test
-	public void testList(){
-		for (AbstractLinkedList<Integer> list : lists) {
-			for (int i=0; i<10000; i++){
-				list.add(i);
-			}
-			Clock c1 = new Clock();
-			for (int i=9999; i>=0; i--){
-				list.remove(i);
-			}
-			c1.stop();
-			for (int i=0; i<10000; i++){
-				list.add(i);
-			}
-			Clock c2 = new Clock();
-			for (int i=0; i<10000; i++){
-				list.remove(i);
-			}
-			c2.stop();
-			assert c1.tookMuchLongerThan(c2) || c2.tookMuchLongerThan(c1) : list.getClass().getName();
-		}
-	}
-	
-	@Test
-	public void testStandardOps(){
-		for (AbstractLinkedList<Integer> list : lists) {
-			try {
-				list.iterator().next();
-				assert false;
-			} catch (NoSuchElementException e){
-			}
-			list.add(17);
-			list.add(17);
-			assert list.size() == 2;
-			try {
-				Iterator<Integer> iter = list.iterator();
-				assert iter.next() == 17;
-				iter.remove();
-				iter.remove();
-			} catch (IllegalStateException e){
-			}
-			assert list.size() == 1;
-			list.clear();
-			assert list.isEmpty();
-		}
-	}
+    
+    private ArrayList<AbstractLinkedList<Integer>> lists;
+    
+    @Before
+    public void setUp() throws Exception {
+        lists = new ArrayList<>();
+        lists.add(new ch.fhnw.algd2.florianfankhauser.LinkedList<Integer>());
+        lists.add(new ch.fhnw.algd2.romangribi.LinkedList<Integer>());
+        lists.add(new ch.fhnw.algd2.christianguedel.LinkedList<Integer>());
+        lists.add(new ch.fhnw.algd2.emanuelmistretta.LinkedList<Integer>());
+        lists.add(new ch.fhnw.algd2.stephenrandles.LinkedList<Integer>());
+        lists.add(new ch.fhnw.algd2.yannickaugstburger.LinkedList<Integer>());
+        lists.add(new ch.fhnw.algd2.mariusdubach.LinkedList<Integer>());
+        lists.add(new ch.fhnw.algd2.stephanbrunner.LinkedList<Integer>());
+        lists.add(new ch.fhnw.algd2.luzius.LinkedList<Integer>()); // add your own LinkedList implementation here
+    }
+    
+    @Test
+    public void testList(){
+        for (AbstractLinkedList<Integer> list : lists) {
+            for (int i=0; i<10000; i++){
+                list.add(i);
+            }
+            Clock c1 = new Clock();
+            for (int i=9999; i>=0; i--){
+                list.remove(i);
+            }
+            c1.stop();
+            for (int i=0; i<10000; i++){
+                list.add(i);
+            }
+            Clock c2 = new Clock();
+            for (int i=0; i<10000; i++){
+                list.remove(i);
+            }
+            c2.stop();
+            assertTrue(list.getClass().getName(), c1.tookMuchLongerThan(c2) || c2.tookMuchLongerThan(c1));
+        }
+    }
+    
+    @Test
+    public void testStandardOps(){
+        for (AbstractLinkedList<Integer> list : lists) {
+            try {
+                list.iterator().next();
+                fail();
+            } catch (NoSuchElementException e){
+            }
+            list.add(17);
+            list.add(17);
+            assertEquals(2, list.size());
+            try {
+                Iterator<Integer> iter = list.iterator();
+                assertEquals(17, iter.next().intValue());
+                iter.remove();
+                iter.remove();
+            } catch (IllegalStateException e){
+            }
+            assertEquals(1, list.size());
+            list.clear();
+            assertTrue(list.isEmpty());
+        }
+    }
 
 }
