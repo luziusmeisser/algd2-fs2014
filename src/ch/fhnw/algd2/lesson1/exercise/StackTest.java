76aa0adb6fa4cc8f989cfb3aa97125b5c0b0a9a1
lukasmusy@gmail.com <lukasmusy@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-02-23 15:33:21 +0000

    lukasmusy included

--- src/ch/fhnw/algd2/lesson1/exercise/StackTest.java
+++ src/ch/fhnw/algd2/lesson1/exercise/StackTest.java
@@ -35,6 +35,7 @@ public class StackTest {
 		stacks.add(new ch.fhnw.algd2.yannickaugstburger.MyStack<String>());
 		stacks.add(new ch.fhnw.algd2.mariusdubach.Stack<String>());
 		stacks.add(new ch.fhnw.algd2.larskessler.Stack<String>());
+		stacks.add(new ch.fhnw.algd2.lukasmusy.Stack<String>());
 //		stacks.add(new ch.fhnw.algd2.YOURNAME.Stack()); add your own stack implementation here
 	}
 
