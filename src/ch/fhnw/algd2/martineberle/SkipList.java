41acf34dd585e7b7031719ec178dd459f4aad755
meberle55@gmail.com <meberle55@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-03-02 15:18:37 +0000

    NOT WORKING -doesn't sort entries correct by value -sometimes goes into infinite loop -doesn't link all pointers

--- src/ch/fhnw/algd2/martineberle/SkipList.java
+++ src/ch/fhnw/algd2/martineberle/SkipList.java
@@ -1,77 +1,91 @@
 package ch.fhnw.algd2.martineberle;
+import java.util.NoSuchElementException;
 import java.util.Random;
 
 import ch.fhnw.algd2.lesson2.exercise.ISkipList;
 
 public class SkipList<T extends Comparable<T>> implements ISkipList<T>{
 Element<T> head;
-Element<T> tail;
-int height;
+//Element<T> tail;
+//int height;
 static final int max = 20;
 
 public SkipList(){
-	height = 0;
+//	height = 0;
 }
 
 	public void updateHeight(Element<T> newElem){
-		if(newElem.pointer.length > height){
-			height = newElem.pointer.length;
-		}
+//		if(newElem.pointer.length > height){
+//			height = newElem.pointer.length;
+//		}
 	}
 	
 	@Override
 	public void add(T item) {
+		Element<T> temp = new Element<T>(item);
 		if(head == null){
-			head = new Element<T>(item, max);
+			head = temp;
+			temp = null;
 		} else {
-			Element<T> temp = new Element<T>(item);
-			insertElement(findInsertPosition(temp));
+			//insertElement(findInsertPosition(temp));
+			sortedInsert(temp);
 			//Find Insert Position - Done!
 			//Create new Element and Calculate pointer lvl			-Done!
 			//Insert into list aka Update pointers in before and after Elements
 		}
 	}
-	public Element<T>[] findInsertPosition(Element<T> newElement){
+	public void sortedInsert(Element<T> newElement){
 		Element<T> curr = head;
 		if(head == null || newElement.val.compareTo(head.val) < 0){ //if new element is smaller than head, create new head.
 			Element<T> tmp = new Element<T>(newElement.val, max);
 			for(int i = 0; i < max; i++){
 				tmp.pointer[i] = head;
-				head = tmp;
-			}			
+			}
+			head = tmp;
+			return;
 		}
-		while(true){
-			if(curr.val.compareTo(newElement.val) <= 0){ //if newElement is bigger or equal than actual compare position
-				int j = 0;
+		//while(true){
+			if(curr.val.compareTo(newElement.val) < 0){ //if newElement is bigger or equal than actual compare position
+				int j = 1;
 				while(curr.pointer.length - j >= 0){
-					if(curr.pointer[curr.pointer.length-j].val.compareTo(newElement.val) <= 0){ //and if newElement is bigger than next element on actual pointerlevel
-						curr = curr.pointer[curr.pointer.length-j]; //iterate curr to highest next available element
+					if(curr.pointer[curr.pointer.length-j] != null && (curr.pointer[curr.pointer.length-j].val).compareTo(newElement.val) < 0){ //and if newElement is bigger than next element on current pointerlevel
+						curr = curr.pointer[curr.pointer.length-j]; //iterate curr to next element
+						j = 1;
 					}
-					else {
-						if(curr.pointer.length-j <= newElement.pointer.length){
+					else if (curr.pointer[curr.pointer.length-j] == null && curr.pointer.length-j <= newElement.pointer.length-1){ //if current pointerlvl is smaller than newElement and has no pointer jet, add newElement
+						curr.pointer[curr.pointer.length-j] = newElement;
+					}
+					
+					if(curr.pointer.length-j <= newElement.pointer.length-1){ //connect all pointerlvls from curr to newElement
+							while(curr.pointer.length-j >=0){
 							newElement.pointer[curr.pointer.length-j] = curr.pointer[curr.pointer.length-j]; //copy pointer from previous element to newElement
 							curr.pointer[curr.pointer.length-j] = newElement;
-						}
-						j++; //if next element on current pointerlvl is smaller than newElement, reduce pointerlevel by 1
+							j++;
+							}
+							return;
 					}
+						j++; //if next element on current pointerlvl is smaller than newElement, reduce pointerlevel by 1
 				}
-							Element<T>[] insertPos = new Element[2];
-							insertPos[0] = curr;
-							insertPos[1] = newElement;
-							return insertPos; //if pointerlvl 0 is reached and newElement is not bigger than next element, position to insert newElement found
-			} else { throw new IllegalStateException("Error finding inserting position!"); //avoid infinite loop
+				//return;
 			}
-		}
+			//else { throw new IllegalStateException("Error finding inserting position!"); //avoid infinite loop
+			//}
+		//}
 	}
 	
 	public void insertElement(Element<T>[] addresses){
-		
+		//after pos found, element already inserted...
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public T removeFirst() {
-		// TODO Auto-generated method stub
-		return null;
+		if(this.head == null){
+			throw new NoSuchElementException("List empty!");
+		}
+		Element<T> tmp = head;
+		head = head.pointer[0];
+		return tmp.val;
 	}
 
 	@Override
@@ -81,11 +95,13 @@ public SkipList(){
 	}
 	
 }
-class Element<T>{
-	T val;
-	Element<T> pointer[];
+class Element<T extends Comparable<T>>{
+	public T val;
+	public Element<T>[] pointer;
+	
+	@SuppressWarnings("unchecked")
 	public Element(T value){
-		val = value;
+		this.val = value;
 		int i = 0;
 		Random r = new Random();
 		boolean moreLinks = true;
@@ -93,11 +109,12 @@ class Element<T>{
 			moreLinks = r.nextBoolean();
 			i++;
 		}
-			pointer = new Element[i];
+			this.pointer = new Element[i];
 	}
-	//separate constructor for first/last element with max pointer height
+	//separate constructor for first element with max pointer height
+	@SuppressWarnings("unchecked")
 	public Element(T value, int height){
-		val = value;
-		pointer = new Element[height];
+		this.val = value;
+		this.pointer = new Element[height];
 	}
 }
