a7ca69cb2def32d051b6cd2fae45a1aa9ef1e66e
marco.gaiffi@gmail.com <marco.gaiffi@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-03-02 22:17:13 +0000

    added marcogaiffi

--- src/ch/fhnw/algd2/lesson2/exercise/SkipListTest.java
+++ src/ch/fhnw/algd2/lesson2/exercise/SkipListTest.java
@@ -34,8 +34,8 @@ public class SkipListTest {
         lists.add(new ch.fhnw.algd2.stephenrandles.SkipList<Integer>());
         lists.add(new ch.fhnw.algd2.christianguedel.SkipList<Integer>());
         lists.add(new ch.fhnw.algd2.larskessler.SkipList<Integer>());
+        lists.add(new ch.fhnw.algd2.marcogaiffi.SkipList<Integer>());
         lists.add(new ch.fhnw.algd2.lukasmusy.SkipList<Integer>());
-
     }
     
     @Test
