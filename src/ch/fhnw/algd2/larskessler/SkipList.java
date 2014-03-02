192715cad2a42b9fe01f9eb94e05400e2385fa7d
l.kessler.one@gmail.com <l.kessler.one@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-03-02 11:24:15 +0000

    skipList first Version

---
+++ src/ch/fhnw/algd2/larskessler/SkipList.java
@@ -0,0 +1,168 @@
+package ch.fhnw.algd2.larskessler;
+
+import java.util.NoSuchElementException;
+
+import ch.fhnw.algd2.lesson2.exercise.ISkipList;
+
+public class SkipList<T extends Comparable<T>> implements ISkipList<T> {
+	
+	private SkipListElement<T> root = new SkipListElement<T>(null);
+	private final static int MAX_HEIGHT = 20;
+
+	@Override
+	public void add(T item) {
+		SkipListElement<T> next = new SkipListElement<T>(item);
+		root.addToList(next, next.getMaxLevel());
+	}
+
+	@Override
+	public T removeFirst() {
+		SkipListElement<T> first = root.removeFirstElement();
+		return first.value;
+	}
+
+	@Override
+	public int countStepsTo(Comparable item) {
+		return root.countStepsTo(item);
+	}
+	
+	class SkipListElement<T extends Comparable<T>> implements Comparable<SkipListElement<T>> {
+		// first element in array is always next element
+		protected SkipListElement<T>[] links;
+		// current element
+		private T value;
+		
+		public SkipListElement(T e) {
+			// set value to new element
+			value = e;
+			
+			if(e == null) {
+				// generate array with max size
+				links = new SkipListElement[MAX_HEIGHT];
+			} else {
+				// generate array with calced size
+				links = new SkipListElement[calcHeight()];
+			}
+		}
+
+		public int countStepsTo(Comparable<T> item) {
+			SkipListElement<T> next = this.links[0];
+			int count = 0;
+			
+			do {
+				// get height of first element
+				next = findNextHop(item, next);
+				// only count correct hops
+				count++;
+				
+			} while(item.compareTo(next.value) != 0);
+			
+			return count;
+		}
+
+		private SkipListElement<T> findNextHop(Comparable<T> item, SkipListElement<T> next) {
+			int count = 0;
+			for(int i = next.getMaxLevel(); i >= 0; i--) {
+				if(next.links[i] != null && item.compareTo(next.links[i].value) >= 0) {
+					next = next.links[i];
+				}
+			}
+			return next;
+		}
+
+		public SkipListElement<T> removeFirstElement() {
+			SkipListElement<T> element = links[0];
+			if(element == null) {
+				// if next first element is null, no such element!
+				throw new NoSuchElementException();
+			} else {
+				// save links of first element to root element
+				for(int i = 0; i < element.links.length; i++) {
+					links[i] = element.links[i];
+				}
+				return element;
+			}			
+		}
+
+		public void addToList(SkipListElement<T> next, int level) {
+			if(level >= 0) {
+				if(links[level] == null) {
+					links[level] = next;
+					addToList(next, level-1);
+				} else if (next.compareTo(links[level]) < 0) {
+					// add element in between or two elements
+					next.links[level] = links[level];
+					links[level] = next;
+					addToList(next, level-1);
+				} else {
+					// add next element
+					links[level].addToList(next, level);
+				}
+			}
+		}
+
+		public int getHeight() {
+			return this.links.length;
+		}
+		
+		public int getMaxLevel() {
+			return this.links.length-1;
+		}
+		
+		/*
+		public void connectTo(SkipListElement<T> e) {
+			for(int i = 0; i < e.getHeight(); i++) {
+				// get next element
+				SkipListElement next = getNext(i);
+				// fill array with next element
+				links[i] = next;
+			}
+		}
+		*/
+		
+		
+		/*
+		private SkipListElement getNext(int level) {
+			// TODO: implement getNext element functionality
+			SkipListElement e = null;
+			int skip = 1;
+			
+			if(level == 1) {
+				e = links[0];
+			} else {
+				
+			}
+			
+			for(int i = 0; i < level; i++) {
+				
+				skip = skip << 1;
+			}
+			
+			return e;
+		}
+		*/
+
+		private int calcHeight() {
+			boolean decide = true;
+			int height = 0;
+			
+			// get next boolean and increment height anyway
+			do {
+				decide = calcBoolean();
+				height++;
+			} while(height <= MAX_HEIGHT && decide == true);
+			
+			return height;
+		}
+
+		private boolean calcBoolean() {
+			return Math.random() < 0.5;
+		}
+
+		@Override
+		public int compareTo(SkipListElement<T> o) {
+			// TODO Auto-generated method stub
+			return value.compareTo(o.value);
+		}
+	}
+}
