42f006a3490571bc390a8cf064c49fdefa235211
stephenruk@gmail.com <stephenruk@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-03-03 12:34:49 +0000

    Binary tree & TreeDetector empty classes

---
+++ trunk/src/ch/fhnw/algd2/stephenrandles/TreeDetector.java
@@ -0,0 +1,19 @@
+// Created by Stephen Randles 03.03.2014
+
+package ch.fhnw.algd2.stephenrandles;
+
+import ch.fhnw.algd2.lesson3.exercise.ITreeDetector;
+import ch.fhnw.algd2.lesson3.exercise.Node;
+
+public class TreeDetector implements ITreeDetector {
+
+	@Override
+	public boolean isTree(Node any) {
+		
+		
+		
+		
+		return false;
+	}
+
+}
