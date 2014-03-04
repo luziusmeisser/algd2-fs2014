5a8428f0afd0f0cd530c42308f5b5d4c8b92835f
l.kessler.one@gmail.com <l.kessler.one@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-03-04 12:47:55 +0000

    neue Version

--- trunk/src/ch/fhnw/algd2/larskessler/TreeDetector.java
+++ trunk/src/ch/fhnw/algd2/larskessler/TreeDetector.java
@@ -2,35 +2,53 @@ package ch.fhnw.algd2.larskessler;
 
 import ch.fhnw.algd2.lesson3.exercise.ITreeDetector;
 import ch.fhnw.algd2.lesson3.exercise.Node;
+
 import java.util.UUID;
 
 public class TreeDetector implements ITreeDetector{
 	
-	// where does current node come from
-	private Node from = null;
-	
+	private static java.util.UUID marker;
+		
 	@Override
 	public boolean isTree(Node any) {
 		Node current = any;
 		Node[] currentNeighbours = current.getNeighbors();
+		marker = UUID.randomUUID();
 		
 		if(currentNeighbours.length == 0) {
 			return true;
 		} else {
-			for(int i = 0; i < currentNeighbours.length; i++) {
-				if(currentNeighbours[i] == from) {
-					i++;
-				} else if (currentNeighbours[i].getMarker() == (Object) true) {
+			return isTreeRec(current, null);
+		}
+	}
+
+	private boolean isTreeRec(Node current, Node from) {
+		current.setMarker(marker);
+		Node[] currentNeighbours = current.getNeighbors();
+	
+		for(int i = 0; i < currentNeighbours.length; i++) {
+			if(currentNeighbours[i] != from) {
+				if(currentNeighbours[i] != null && currentNeighbours[i].getMarker() == marker) {
 					return false;
-				} else if (currentNeighbours[i].getMarker() == (Object) false) {
-					this.from = current;
-					currentNeighbours[i].setMarker(true);
-					current = currentNeighbours[i];
-					isTree(current);
+				} else if(!isTreeRec(currentNeighbours[i], current)) {
+					return false;
 				}
 			}
+			
+			/*
+			if(currentNeighbours[i] == from) {
+				// do nothing (just increment)
+			} else if (currentNeighbours[i].getMarker() == (Object) marker) {
+				return false;
+			} else {
+				from = current;
+				currentNeighbours[i].setMarker(marker);
+				if(!isTreeRec(currentNeighbours[i], from)) {
+					return false;
+				}
+			}
+			*/
 		}
 		return true;
 	}
-	
 }
