6b2b6d5b7490284006d1eee66308a31f75af44b4
stephenruk@gmail.com <stephenruk@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-03-10 18:18:35 +0000

    Scrapped my broken recursive breadth-first search. Implemented breadth-first search with a queue, so the tree is traversed level by level.

--- trunk/src/ch/fhnw/algd2/stephenrandles/BinaryTreeTraverser.java
+++ trunk/src/ch/fhnw/algd2/stephenrandles/BinaryTreeTraverser.java
@@ -2,6 +2,9 @@
 
 package ch.fhnw.algd2.stephenrandles;
 
+import java.util.Queue;
+import java.util.LinkedList;
+
 import ch.fhnw.algd2.lesson4.exercise.BinaryNode;
 import ch.fhnw.algd2.lesson4.exercise.IBinaryTreeTraverser;
 
@@ -10,28 +13,35 @@ public class BinaryTreeTraverser implements IBinaryTreeTraverser {
 	@Override
 	public String assemble(BinaryNode root, boolean depthFirst) {
 		if (depthFirst) {
-			return walkNodeDeepFirst(root);			
+			return assembleDepthFirst(root);			
 		} else {
-			return walkNode(root);
+			return assembleBreadthFirst(root);
 		}
 	}
 	
-	private String walkNode(BinaryNode node) {
-		if (node != null) {
-			String s = "";
+	private String assembleBreadthFirst(BinaryNode root) {
+		StringBuilder sb = new StringBuilder();
+		Queue<BinaryNode> q = new LinkedList<BinaryNode>();
+		BinaryNode node;
+		
+		q.add(root);		
+		
+		while (!q.isEmpty()) {
+			node = q.poll();
+			sb.append(node.getValue());
+
 			if (node.getLeftChild() != null)
-				s += node.getLeftChild().getValue();
-			if (node.getRightChild() != null) {
-				s += node.getRightChild().getValue();
-			}
-			return walkNode(node.getLeftChild()) + walkNode(node.getRightChild()) + s;
-		}
-		return "";
+				q.add(node.getLeftChild());
+			if (node.getRightChild() != null)
+				q.add(node.getRightChild());
+		}		
+		
+		return sb.toString();		
 	}
 	
-	private String walkNodeDeepFirst(BinaryNode node) {
+	private String assembleDepthFirst(BinaryNode node) {
 		if (node != null) {
-			return walkNodeDeepFirst(node.getLeftChild()) + node.getValue() + walkNodeDeepFirst(node.getRightChild());
+			return assembleDepthFirst(node.getLeftChild()) + node.getValue() + assembleDepthFirst(node.getRightChild());
 		}
 		return "";
 	}
