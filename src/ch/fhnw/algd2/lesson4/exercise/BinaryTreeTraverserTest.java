713613150d875f564b3d502ac2ec9552aba7e518
l.kessler.one@gmail.com <l.kessler.one@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-03-10 12:40:59 +0000

    resolved conflicts

--- src/ch/fhnw/algd2/lesson4/exercise/BinaryTreeTraverserTest.java
+++ src/ch/fhnw/algd2/lesson4/exercise/BinaryTreeTraverserTest.java
@@ -15,6 +15,8 @@ public class BinaryTreeTraverserTest {
 	public void setUp() throws Exception {
 		lists = new ArrayList<>();
 		// add your own implementation here
+		//lists.add(new ch.fhnw.algd2.luzius.BinaryTreeTraverser());
+		//lists.add(new ch.fhnw.algd2.larskessler.BinaryTreeTraverser());
 		lists.add(new ch.fhnw.algd2.florianfankhauser.lesson4.BinaryTreeTraverser());
 		lists.add(new ch.fhnw.algd2.emanuelmistretta.BinaryTreeTraverser());
         lists.add(new ch.fhnw.algd2.stephanbrunner.BinaryTreeTraverser());
