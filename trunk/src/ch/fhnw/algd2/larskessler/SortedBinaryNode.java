bd2c8c23d57acb79b7401efe128d2acd5c4903c8
l.kessler.one@gmail.com <l.kessler.one@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-03-11 09:05:58 +0000

    initial version

---
+++ trunk/src/ch/fhnw/algd2/larskessler/SortedBinaryNode.java
@@ -0,0 +1,24 @@
+package ch.fhnw.algd2.larskessler;
+
+import ch.fhnw.algd2.lesson4.exercise.AbstractSortedBinaryNode;
+
+public class SortedBinaryNode extends AbstractSortedBinaryNode {
+
+	public SortedBinaryNode(String value) {
+		super(value);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int insert(String value) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void remove(String value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
