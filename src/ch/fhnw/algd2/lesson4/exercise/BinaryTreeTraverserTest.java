9f50378c72bd020dd759e222dbf5c875d9b62cf3
marco.gaiffi@gmail.com <marco.gaiffi@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-03-16 21:35:29 +0000

    added marcogaiffi

--- src/ch/fhnw/algd2/lesson4/exercise/BinaryTreeTraverserTest.java
+++ src/ch/fhnw/algd2/lesson4/exercise/BinaryTreeTraverserTest.java
@@ -26,6 +26,7 @@ public class BinaryTreeTraverserTest {
         lists.add(new ch.fhnw.algd2.stephenrandles.lesson04.BinaryTreeTraverser());
         lists.add(new ch.fhnw.algd2.martineberle.BinaryTreeTraverser());
 		lists.add(new ch.fhnw.algd2.christianguedel.BinaryTreeTraverser());
+		lists.add(new ch.fhnw.algd2.marcogaiffi.BinaryTreeTraverser());
 	}
 
 	@Test
