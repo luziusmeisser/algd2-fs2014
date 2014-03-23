62b273cf9fa283d678a176c85d19928715886393
marco.gaiffi@gmail.com <marco.gaiffi@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-03-23 21:11:48 +0000

    exercise 5 solution

---
+++ src/ch/fhnw/algd2/marcogaiffi/AVLNode.java
@@ -0,0 +1,67 @@
+// Created by Marco on 17.03.2014
+
+package ch.fhnw.algd2.marcogaiffi;
+
+import ch.fhnw.algd2.lesson5.exercise.AbstractAVLNode;
+
+
+public class AVLNode extends AbstractAVLNode{
+	
+	public AVLNode root = new AVLNode(null);
+	public String value;
+	public int height;
+	public AVLNode left, right;
+
+	//initial height = 1
+	public AVLNode(String value) {
+		super(value);
+	}
+
+	@Override
+	protected AbstractAVLNode createNode(String value) {
+		return new AVLNode(value);
+	}
+
+	@Override
+	protected int calculateHeight() {
+		return Math.max(getLeftHeight(), getRightHeight() + 1);
+	}
+
+	@Override
+	protected AbstractAVLNode ensureBalance() {
+		if(getBalance() > 1){
+			//left right case
+			if(getLeftChild().getBalance() == -1){
+				setLeft(getLeftChild().rotateLeft()); //reduce to left left case
+			}
+			//left left case
+			return rotateRight();
+		}else if (getBalance() < -1){
+			//right left case
+			if(getRightChild().getBalance() == 1){ //reduce to right right case
+				setRight(getRightChild().rotateRight());
+			}
+			//right right case
+			return rotateLeft();
+		}
+		else{
+			return this;
+		}
+	}
+	
+	@Override
+	public AbstractAVLNode rotateLeft() {
+		AbstractAVLNode right = getRightChild();
+		this.setRight(getRightChild().getLeftChild());
+		right.setLeft(this);
+		return right;
+	}
+
+	@Override
+	public AbstractAVLNode rotateRight() {
+		AbstractAVLNode left = getLeftChild();
+		this.setRight(getRightChild().getLeftChild());
+		right.setLeft(this);
+		return right;
+	}
+}
