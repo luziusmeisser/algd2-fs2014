0f4cfa3cf86f7e349683f85de1731c5708998766
luzius.meisser@gmail.com <luzius.meisser@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-03-24 13:49:39 +0000

    heap implementation

--- trunk/src/ch/fhnw/algd2/luzius/Heap.java
+++ trunk/src/ch/fhnw/algd2/luzius/Heap.java
@@ -2,32 +2,81 @@
 
 package ch.fhnw.algd2.luzius;
 
-import java.util.concurrent.PriorityBlockingQueue;
-
 import ch.fhnw.algd2.lesson6.exercise.IHeap;
 
 public class Heap implements IHeap {
-	
-	// Not an acceptable implementation! For testing puposes only.
-	private PriorityBlockingQueue<String> queue;
 
-	public Heap(){
-		this.queue = new PriorityBlockingQueue<>();
-	}
-	
-	@Override
-	public void offer(String s) {
-		this.queue.offer(s);
+	private int pos;
+	private String[] queue;
+
+	public Heap() {
+		this.queue = new String[1000];
+		this.pos = 0;
 	}
 
 	@Override
-	public String peek() {
-		return queue.peek();
+	public synchronized void offer(String s) {
+		queue[pos++] = s;
+		siftUp(pos - 1);
+	}
+
+	private void siftUp(int child) {
+		int parent = getParent(child);
+		while (parent >= 0 && queue[child].compareTo(queue[parent]) < 0) {
+			String earlier = queue[child];
+			queue[child] = queue[parent];
+			queue[parent] = earlier;
+			child = parent;
+			parent = getParent(child);
+		}
+	}
+
+	private int getParent(int child) {
+		return (child - 1) / 2;
 	}
 
 	@Override
-	public String poll() {
-		return queue.poll();
+	public synchronized String peek() {
+		if (pos == 0) {
+			return null;
+		} else {
+			return queue[0];
+		}
+	}
+
+	@Override
+	public synchronized String poll() {
+		if (pos == 0) {
+			return null;
+		} else {
+			String first = queue[0];
+			queue[0] = queue[--pos];
+			siftDown(0);
+			return first;
+		}
+	}
+
+	private void siftDown(int parent) {
+		int leftC = parent * 2 + 1;
+		int rightC = leftC + 1;
+		if (leftC >= pos){
+			return;
+		} else if (rightC >= pos){
+			if (queue[parent].compareTo(queue[leftC]) > 0){
+				String t = queue[leftC];
+				queue[leftC] = queue[parent];
+				queue[parent] = t;
+			}
+			return;
+		} else {
+			int lowerC = queue[leftC].compareTo(queue[rightC]) < 0 ? leftC : rightC;
+			if (queue[parent].compareTo(queue[lowerC]) > 0){
+				String t = queue[lowerC];
+				queue[lowerC] = queue[parent];
+				queue[parent] = t;
+				siftDown(lowerC);
+			}
+		}
 	}
 
 }
