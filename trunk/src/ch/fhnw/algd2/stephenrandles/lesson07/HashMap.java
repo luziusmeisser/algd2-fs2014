0b20b4e20c4a808abdf30773b3c40695247d2430
stephenruk@gmail.com <stephenruk@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-04-01 08:17:58 +0000

    Attempt at collision-handling HashMap, by storing elements with same hash in a linked list

--- trunk/src/ch/fhnw/algd2/stephenrandles/lesson07/HashMap.java
+++ trunk/src/ch/fhnw/algd2/stephenrandles/lesson07/HashMap.java
@@ -5,26 +5,71 @@ package ch.fhnw.algd2.stephenrandles.lesson07;
 import ch.fhnw.algd2.lesson7.exercise.IHashMap;
 
 public class HashMap implements IHashMap {
-	private String[] values;
+	private Element[] values;
 	
 	private final int ARRAY_SIZE = 1000;
 
 	public HashMap() {
-		values = new String[ARRAY_SIZE]; 
+		values = new Element[ARRAY_SIZE]; 
 	}
 
 	@Override
 	public void put(String key, String value) {
-		values[calcIndex(key)] = value;
+		int index = calcIndex(key);
+		
+		Element e = values[index];
+		if (e == null) {
+			values[index] = new Element(key, value);
+		} else {
+			while (e.hasNext() && e.key != key) {
+				e = e.next;
+			}
+			
+			if (e.key != key) {
+				e.next = new Element(key, value);
+			}			
+		}
 	}
 
 	@Override
 	public String get(String key) {
-		return values[calcIndex(key)];
+		int index = calcIndex(key);
+		Element e = values[index];
+		
+		if (e == null) 		return null;
+		
+		while (e.hasNext() && e.key != key) {
+			e = e.next;
+		}
+		
+		if (e.key == key)	return e.value;
+		else				return null;
 	}
 	
 	private int calcIndex(String key) {
 		return key.hashCode() % values.length;
+	}
+	
+	
+	
+	class Element {
+		String key;
+		String value;
+		Element next;
+		
+		public Element(String key, String value) {
+			this.set(key,value);
+		}
+		
+		void set(String key, String contents) {
+			this.key = key;
+			this.value = contents; 
+		}
+		
+		boolean hasNext() {
+			return next != null;
+		}
+		
 	}
 
 }
