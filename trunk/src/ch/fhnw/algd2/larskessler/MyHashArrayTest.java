457602b26ae6018b1e2876104d260b352b168133
l.kessler.one@gmail.com <l.kessler.one@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-04-01 14:30:29 +0000

    updated test

--- trunk/src/ch/fhnw/algd2/larskessler/MyHashArrayTest.java
+++ trunk/src/ch/fhnw/algd2/larskessler/MyHashArrayTest.java
@@ -9,8 +9,12 @@ public class MyHashArrayTest {
 		String value1 = "Eins";
 		
 		String key2 = "71443";
+		// String key2 = "7144453";
 		String value2 = "Zwei";
 		
+		String key3 = "2";
+		
+		// put element in array
 		System.out.println(key1.hashCode() % 1000);
 		arr.put(key1, value1);
 		String get1 = arr.get(key1);
@@ -18,10 +22,18 @@ public class MyHashArrayTest {
 		
 		System.out.print("\n");
 		
+		// element same position
 		System.out.println(key2.hashCode() % 1000);
 		arr.put(key2, value2);
 		String get2 = arr.get(key2);
 		System.out.println(get2);
+		
+		System.out.print("\n");
+		
+		// element not in array
+		System.out.println(key3.hashCode() % 1000);
+		String get3 = arr.get(key3);
+		System.out.println(get3);
 	}
 
 }
