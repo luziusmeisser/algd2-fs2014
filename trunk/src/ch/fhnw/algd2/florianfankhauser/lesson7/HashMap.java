41607e952abee458f75835af1a91b24bdd574e89
fankiflo@gmail.com <fankiflo@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-04-12 11:59:24 +0000

    HashMap implemented

--- trunk/src/ch/fhnw/algd2/florianfankhauser/lesson7/HashMap.java
+++ trunk/src/ch/fhnw/algd2/florianfankhauser/lesson7/HashMap.java
@@ -2,33 +2,57 @@
 
 package ch.fhnw.algd2.florianfankhauser.lesson7;
 
+import java.util.NoSuchElementException;
+
 import ch.fhnw.algd2.lesson7.exercise.IHashMap;
 
 public class HashMap implements IHashMap {
 	private static final int MAP_SIZE = 1000;
-	private String[] elements = new String[MAP_SIZE];
+	private MapElement[] elements = new MapElement[MAP_SIZE];
 
 	@Override
 	public void put(String key, String value){
 		int index = calculateIndex(key);
+		MapElement el = new MapElement(key, value);
 		if (elements[index] != null) {
-			throw new IllegalStateException();
+			el.next = elements[index];
 		}
-		elements[index] = value;
+		elements[index] = el;
 	}
 
 	@Override
 	public String get(String key) {
-		return elements[calculateIndex(key)];
+		int index = calculateIndex(key);
+		MapElement el = elements[index];
+		if (el.next != null) {
+			return findExact(key, el).value;
+		}
+		return el.value;
 	}
 	
 	private int calculateIndex(String key) {
-//		return Math.abs(key.hashCode()) % 1000;
 		long index = 1; 
 		for (int i = 0; i < key.length(); i++) {
 			index += key.charAt(i);
 		}
 		index = index % 1000;
 		return (int) index;
+	}
+
+	private MapElement findExact(String key, MapElement el) {
+		if (el == null) throw new NoSuchElementException();
+		if (el.key.equals(key)) return el;
+		return findExact(key, el.next);
+	}
+	
+	private class MapElement {
+		private String key;
+		private String value;
+		private MapElement next;
+		
+		private MapElement(String key, String value) {
+			this.value = value;
+			this.key = key;
+		}
 	}
 }
