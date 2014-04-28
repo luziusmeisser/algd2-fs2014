475ceea3067b948cd355130a086a93f50ebc41f6
luzius.meisser@gmail.com <luzius.meisser@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-04-28 10:02:18 +0000

    lesson 8

---
+++ src/ch/fhnw/algd2/lesson8/IHashMap2.java
@@ -0,0 +1,24 @@
+// Created by Luzius on 31.03.2014
+
+package ch.fhnw.algd2.lesson8;
+
+/**
+ * Exercise 8:
+ * 
+ * HashMap with rehashing.
+ * 
+ * Similar to exercise 7, we implement a hash map, but this time with re-hashing instead of
+ * using linked lists. Also, we want to support removes this time.
+ * 
+ * Initialize the HashMap with a size of 1000. You can assume that this is enough and do
+ * not need to implement resize.
+ */
+public interface IHashMap2 {
+	
+	public void put(String key, String value);
+	
+	public String get(String key);
+
+	public String remove(String key);
+
+}
