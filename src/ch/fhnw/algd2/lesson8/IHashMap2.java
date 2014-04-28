88c2b4c983e16ba131c9b5c9d419075d88cd978f
luzius.meisser@gmail.com <luzius.meisser@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-04-28 11:30:45 +0000

    test

--- src/ch/fhnw/algd2/lesson8/IHashMap2.java
+++ src/ch/fhnw/algd2/lesson8/IHashMap2.java
@@ -12,6 +12,9 @@ package ch.fhnw.algd2.lesson8;
  * 
  * Initialize the HashMap with a size of 1000. You can assume that this is enough and do
  * not need to implement resize.
+ * 
+ * Visualization: http://www.cs.usfca.edu/~galles/visualization/ClosedHash.html
+ * (Unlike this visualization, we do not allow multiple identical keys.)
  */
 public interface IHashMap2 {
 	
