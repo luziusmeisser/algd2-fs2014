fdc62a8f34d94eac648f6cd1703788e085146c90
l.kessler.one@gmail.com <l.kessler.one@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-05-01 18:31:34 +0000

    working hashmap2

--- trunk/src/ch/fhnw/algd2/larskessler/HashMap2.java
+++ trunk/src/ch/fhnw/algd2/larskessler/HashMap2.java
@@ -1,126 +1,99 @@
 package ch.fhnw.algd2.larskessler;
-
 import ch.fhnw.algd2.lesson8.IHashMap2;
 
 public class HashMap2 implements IHashMap2 {
 	
-	private final static int RESIZE = 80;
-	private final static float FACTOR = 1.5f;
-	private HashMapElement[] m_HashTable;
-	private int m_m;
-	private int fillStatus;
+	private int size;
+	private int count;
+	private static final float FACTOR = 2.0f;
+	private static final int PERCENTAGE = 90;
+	private HashMapElement[] elements;
 	
 	public HashMap2() {
-		this.m_m = 1000;
-		m_HashTable = new HashMapElement[m_m];
+		this.size = 1000;
+		this.elements = new HashMapElement[this.size];
 	}
 
 	@Override
 	public void put(String key, String value) {
-		resize();
-		add(calcPos(key), key, value);
+		resizeStorage();
+		elements[getGap(calcPos(key), key)] = new HashMapElement(key, value);
+		this.count++;
 	}
 
 	@Override
 	public String get(String key) {
-		HashMapElement item = getElement(key);
-		if(item == null) {
-			return null;
-		} else {
-			return getElement(key).m_data;
-		}
+		return getElement(key).data;
 	}
 
 	@Override
 	public String remove(String key) {
-		HashMapElement item = getElement(key);
+		HashMapElement element = getElement(key);
 		
-		if(item != null) {
-			fillStatus--;
-			item.m_deleted = true;
-			return item.m_data;
+		if(element != null) {
+			element.removed = true;
+			this.count--;
+			return element.data;
 		}
-		
 		return null;
-	}
-	
-	private void resize() {
-		if(fillStatus > ((this.m_m / 100) * RESIZE)) {
-			HashMapElement[] oldElements = m_HashTable;
-			int size = (int) (m_m * FACTOR);
-			m_HashTable = new HashMapElement[size];
-			
-			for(int i = 0; i < m_m; i++) {
-				if(oldElements[i] != null && !oldElements[i].m_deleted) {
-					m_HashTable[i] = oldElements[i];
-				}
-			}
-			m_m = size;
-		}
 	}
 	
 	private int calcPos(String key) {
 		int hashCode = key.hashCode();
-		int pos = hashCode % m_m;
+		int pos = hashCode % this.size;
 		
 		// avoid negative array pos
 		if(pos < 0)  pos = pos * (-1);
-		
-		//System.out.println(pos);
 		return pos;
 	}
 	
-	private HashMapElement add(int pos, String key, String value) {
-		HashMapElement item = new HashMapElement(key, value);
-		fillStatus++;
-		
-		if(m_HashTable[pos] == null) {
-			return m_HashTable[pos] = item;
+	private int getGap(int pos, String key) {
+		if(this.elements[pos] == null) {
+			return pos;
 		} else {
-			return m_HashTable[findNextGap(pos, key)] = item;
-		}
-	}
-	
-	private int findNextGap(int pos, String key) {
-		while(pos < m_m-1 && m_HashTable[pos] != null && !m_HashTable[pos].m_key.equals(key) && !m_HashTable[pos].m_deleted) {			
-			pos++;
+			while(pos < this.size-1 && this.elements[pos] != null && !this.elements[pos].removed && !this.elements[pos].equals(key)) {
+				pos++;
+			}
 		}
 		return pos;
 	}
 	
 	private HashMapElement getElement(String key) {
 		int pos = calcPos(key);
-		
-		if(m_HashTable[pos] == null) {
-			return null;
-		} else {
-			while(pos < m_m && m_HashTable[pos] != null) {
-				if(m_HashTable[pos].equals(key)) {
-					return m_HashTable[pos];
-				}
-				pos++;
+		while(pos < this.size && this.elements[pos] != null && !this.elements[pos].key.equals(key)) {
+			pos++;
+		}
+		if(this.elements[pos] != null && this.elements[pos].key.equals(key)) {
+			return this.elements[pos];
+		}
+		return null;
+	}
+	
+	private void resizeStorage() {
+		if(this.count > ((this.size / 100) * PERCENTAGE)) {
+			HashMapElement[] oldElements = this.elements;
+			this.elements = new HashMapElement[(int) (oldElements.length * FACTOR)];
+			for (HashMapElement e : oldElements) {
+				if (e != null && !e.removed)
+					put(e.key, e.data);
 			}
-			return null;
+			this.size = this.elements.length;
 		}
 	}
 	
-	class HashMapElement {
-		private String m_data;
-		private String m_key;
-		private boolean m_deleted;
+	public class HashMapElement {
+		private String key;
+		private String data;
+		private boolean removed;
 		
 		public HashMapElement(String key, String data) {
-			m_data = data;
-			m_key = key;
-			m_deleted = false;
+			this.key = key;
+			this.data = data;
+			this.removed = false;
 		}
 		
-		public boolean equals(Object o) {
-			return m_key.equals(((HashMapElement)o).m_key);
+		public boolean equals(String key) {
+			return this.key.equals(key);
 		}
-		
-		public boolean equals(String key){
-			return m_key.equals(key);
-		}
-	} 
+	}
 }
