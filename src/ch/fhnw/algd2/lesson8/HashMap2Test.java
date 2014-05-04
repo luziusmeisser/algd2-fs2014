439198d9ec81b810d86dc87d7edc1c22ae5d3ef7
r.gribii@gmail.com <r.gribii@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-05-04 16:00:19 +0000

    - Implemented Exercise 8 (HashMap2)

--- src/ch/fhnw/algd2/lesson8/HashMap2Test.java
+++ src/ch/fhnw/algd2/lesson8/HashMap2Test.java
@@ -25,7 +25,8 @@ public class HashMap2Test {
 		lists.add(new ch.fhnw.algd2.mariusdubach.lesson8.HashMap2());
         lists.add(new ch.fhnw.algd2.stephanbrunner.HashMap2());
         lists.add(new ch.fhnw.algd2.kevinwieser.HashMap2());
-                
+        lists.add(new ch.fhnw.algd2.romangribi.HashMap2());
+        
 	}
 	
 	@Test
