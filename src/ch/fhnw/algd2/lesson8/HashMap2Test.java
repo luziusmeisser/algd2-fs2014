38155255d5a780e313cf80a62d241d2cc70553f2
lukasmusy@gmail.com <lukasmusy@gmail.com@723ec295-799f-11cb-119f-f68fbc68e5b8>
2014-05-04 20:54:33 +0000

    lukasmusy added

--- src/ch/fhnw/algd2/lesson8/HashMap2Test.java
+++ src/ch/fhnw/algd2/lesson8/HashMap2Test.java
@@ -25,6 +25,7 @@ public class HashMap2Test {
 		lists.add(new ch.fhnw.algd2.mariusdubach.lesson8.HashMap2());
         lists.add(new ch.fhnw.algd2.stephanbrunner.HashMap2());
         lists.add(new ch.fhnw.algd2.christianguedel.HashMap2());
+        lists.add(new ch.fhnw.algd2.lukasmusy.HashMap2());
         lists.add(new ch.fhnw.algd2.kevinwieser.HashMap2());
         lists.add(new ch.fhnw.algd2.romangribi.HashMap2());
         lists.add(new ch.fhnw.algd2.martineberle.HashMap2());
